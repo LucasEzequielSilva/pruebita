0d31d0ef393e67999bb517f81b6f495a7f0622bc
lucasidiotlab <144950728+lucasidiotlab@users.noreply.github.com>
2023-11-12 16:59:37 -0300

    changes

--- src/app/bottom-sheet/bottom-sheet.component.ts
+++ src/app/bottom-sheet/bottom-sheet.component.ts
@@ -10,10 +10,9 @@ export class BottomSheetComponent {
   private startY = 30;
   private initialHeight = 30;
 
-  readonly minHeight = 100; // Altura mínima permitida
-  readonly maxHeight = 75; // Altura máxima permitida (en vh)
-  readonly openThreshold = 1; // Ajusta según tus necesidades
-  readonly closedThreshold = 1; // Puedes ajustar según tus necesidades
+  readonly minHeight = 100;
+  readonly openThreshold = 1;
+  readonly closedThreshold = 1;
   @ViewChild('bottomSheet', { static: true }) bottomSheet!: ElementRef;
 
   constructor(private elementRef: ElementRef) {}
@@ -28,94 +27,87 @@ export class BottomSheetComponent {
     this.onDragEnd(event);
   }
 
-  onDragStart(event: MouseEvent): void {
-    event.preventDefault();
+  @HostListener('document:touchmove', ['$event'])
+  onDocumentTouchMove(event: TouchEvent): void {
+    if (event.touches && event.touches.length === 1) {
+      this.onDragMove(event.touches[0]);
+      event.preventDefault();
+    }
+  }
+
+  @HostListener('document:touchend', ['$event'])
+  onDocumentTouchEnd(event: TouchEvent): void {
+    if (event.touches && event.touches.length === 1) {
+      this.onDragEnd(event.touches[0]);
+      event.preventDefault();
+    }
+  }
+
+  onDragStart(event: MouseEvent | Touch): void {
     this.isDragging = true;
-    this.startY = event.clientY;
-    // Establecer la altura inicial como la altura actual del bottomSheet
+    this.startY = ('clientY' in event ? event.clientY : (event as Touch).clientY) || 0;
     this.initialHeight = this.bottomSheet.nativeElement.clientHeight;
     console.log("arranca", this.startY, this.initialHeight);
   }
-  
 
-  onDragMove(event: MouseEvent): void {
+  onDragMove(event: MouseEvent | Touch): void {
     if (this.isDragging) {
-      const mouseY = event.clientY;
-    
-      // Calcular la posición relativa del mouse en el viewport
-      const relativePosition = mouseY / window.innerHeight;
-    
-      // Calcular la nueva altura en base a la posición relativa y los límites
-      const newHeight = window.innerHeight - mouseY; // Puedes ajustar esto según tus necesidades
-    
-      // Limitar la altura superior e inferior
+      const mouseY = ('clientY' in event ? event.clientY : (event as Touch).clientY) || 0;
+      const newHeight = window.innerHeight - mouseY;
       const clampedHeight = Math.max(this.minHeight, Math.min(newHeight, window.innerHeight - this.minHeight));
-    
-      // Aplicar el cambio de altura al elemento #bottomSheet
       this.bottomSheet.nativeElement.style.height = `${clampedHeight}px`;
-  
-      // Verificar si se está realizando un arrastre para aplicar animación
+
       if (this.isDragging) {
         console.log("moviendo");
       }
     }
   }
-  
-onDragEnd(event: MouseEvent): void {
-  if (this.isDragging) {
-    this.isDragging = false;
 
-    // Calcular la diferencia entre la posición inicial y final del drag
-    const dragDistance = this.startY - event.clientY;
+  onDragEnd(event: MouseEvent | Touch): void {
+    if (this.isDragging) {
+      this.isDragging = false;
+      const dragDistance = this.startY - ('clientY' in event ? event.clientY : (event as Touch).clientY) || 0;
+      const isDragUp = dragDistance > 0;
+      let finalHeight: number;
 
-    // Determinar si el arrastre fue hacia arriba o hacia abajo
-    const isDragUp = dragDistance > 0;
+      if (!isDragUp && dragDistance > this.closedThreshold) {
+        finalHeight = this.minHeight;
+      } else {
+        const isOpen = this.bottomSheet.nativeElement.clientHeight > this.minHeight;
+        finalHeight = isDragUp ? window.innerHeight : (isOpen ? window.innerHeight : this.minHeight);
+      }
 
-    // Calcular la altura final basada en la dirección del arrastre y el estado actual
-    let finalHeight: number;
+      if ((isDragUp && !(this.bottomSheet.nativeElement.clientHeight > this.minHeight)) || (!isDragUp && (this.bottomSheet.nativeElement.clientHeight > this.minHeight))) {
+        this.animateHeight(this.bottomSheet.nativeElement.clientHeight, finalHeight);
+      } else {
+        this.bottomSheet.nativeElement.style.height = `${finalHeight}px`;
+      }
 
-    // Verificar si se cumple el closedThreshold para cerrar
-    if (!isDragUp && dragDistance > this.closedThreshold) {
-      finalHeight = this.minHeight;
-    } else {
-      const isOpen = this.bottomSheet.nativeElement.clientHeight > this.minHeight;
-      finalHeight = isDragUp ? window.innerHeight : (isOpen ? window.innerHeight : this.minHeight);
+      console.log("termina");
     }
-
-    // Animar o establecer la altura sin animación según sea necesario
-    if ((isDragUp && !(this.bottomSheet.nativeElement.clientHeight > this.minHeight)) || (!isDragUp && (this.bottomSheet.nativeElement.clientHeight > this.minHeight))) {
-      this.animateHeight(this.bottomSheet.nativeElement.clientHeight, finalHeight);
-    } else {
-      this.bottomSheet.nativeElement.style.height = `${finalHeight}px`;
-    }
-
-    console.log("termina");
   }
-}
-       
+
   animateHeight(startHeight: number, targetHeight: number): void {
-    const duration = 300; // Duración de la animación en milisegundos
+    const duration = 300;
     const startTime = performance.now();
-  
+
     const animate = (time: number) => {
       let progress = (time - startTime) / duration;
-  
+
       if (progress > 1) {
         progress = 1;
       }
-  
+
       const easing = progress > .5 ? 1 - Math.sqrt(1 - (progress * 2 - 2) ** 2) : (progress * 2) ** 2 / 2;
       const newHeight = startHeight + (targetHeight - startHeight) * easing;
-  
+
       this.bottomSheet.nativeElement.style.height = `${newHeight}px`;
-  
+
       if (progress < 1) {
         requestAnimationFrame(animate);
       }
     };
-  
+
     requestAnimationFrame(animate);
   }
-  
-  
 }
